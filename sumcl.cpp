19b82e8fd80956cc38d1bb77ebe12055579a3ae5
AalyaIqbal <63206070+AalyaIqbal@users.noreply.github.com>
2020-08-26 21:12:45 +0530

    Add files via upload

---
+++ sumcl.cpp
@@ -0,0 +1,12 @@
+//program to calculate the sum of 2 numbers
+#include<iostream>
+using namespace std;
+int main(){
+	int no1, no2, sum;
+	cout<<"enter two numbers:\n";
+	cin>>no1>>no2;
+	cout<<endl<<"Sum of two numbers=";
+	sum=no1+no2;
+	cout<<sum;
+	return 0;
+}
