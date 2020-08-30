2aa20aa614ec8051fd23dd9cc460a34a1ac7ba6e
AalyaIqbal <63206070+AalyaIqbal@users.noreply.github.com>
2020-08-30 12:23:00 +0530

    Add files via upload

---
+++ greatestbitwiseop.cpp
@@ -0,0 +1,15 @@
+//program to find the greatest of three numbers in c++
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2,num3;
+	cout<<"Input three numbers"<<endl;
+	cin>>num1>>num2>>num3;
+	if(num1>num2&&num1>num3)
+	    cout<<"The greatest number is "<<num1;
+	else if(num2>num1&&num2>num3)
+		cout<<"The greatest number is "<<num2;
+		else
+		cout<<"The greatest number is "<<num3;
+		return 0;
+	}
